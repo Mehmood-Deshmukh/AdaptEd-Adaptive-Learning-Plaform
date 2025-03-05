28f224511ce507ff9161fb2318d1ae7f9ae30590
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2025-03-05 15:59:14 +0530

    add : basic layout for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
+
 
 function App() {
 
   return (
-    <>
-    <div>
-      <h1> Hello, world! </h1>
-    </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
