e1103ae7b06a6d7583db4589de00ea66abc95481
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2025-03-24 21:15:48 +0530

    feat: connected learning parameters survey to backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController');
 const authenticateUser = require("../middlewares/auth")
 const router = express.Router();
 
+router.post('/update-learning-parameters', authenticateUser, userController.updateLearningParameters);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/forgot-password', userController.forgotPassword);
@@ -10,5 +11,6 @@ router.post('/reset-password', userController.resetPassword);
 router.get('/check-auth', userController.checkAuth);
 router.post('/join-community', authenticateUser, userController.joinCommunity);
 router.post('/leave-community', authenticateUser, userController.leaveCommunity);
+
 
 module.exports = router;
--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 cd client
+npm i
 npm run dev &
 
 cd ../server
+npm i
 nodemon index.js &
 
 cd ../python-server
 source .venv/bin/activate
+pip -r install requirements.txt
 python server.py
